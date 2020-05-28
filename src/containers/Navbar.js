eaab4a840b1b4f5c7178064a5ab7411e3ef7e3e1
Panupak Vichaidit <newnok6@gmail.com>
2020-05-28 21:54:53 +0700

    [🎉] add the nav bar menu

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -40,11 +40,11 @@ class navigation extends Component {
         <ul className="navigation__container">
           <NavigationItem link="/" exact><img className="navigation__container--logo" src={NetflixLogo} alt="" /></NavigationItem>
           <DropdownArrow className="navigation__container--downArrow-2"></DropdownArrow>
-          {/* <div className="navigation__container-link pseudo-link">Home</div>
+          <div className="navigation__container-link pseudo-link">Home</div>
           <div className="navigation__container-link pseudo-link">TV Shows</div>
           <div className="navigation__container-link pseudo-link">Movies</div>
           <div className="navigation__container-link pseudo-link">Recently Added</div>
-          <div className="navigation__container-link pseudo-link">My List</div> */}
+          <div className="navigation__container-link pseudo-link">My List</div>
 
 
           <div className="navigation__container--left">
